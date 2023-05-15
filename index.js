d6b9db9d4fca78b25fb45f81130834e55e19b30b
jshimetz <jeremyshimetz@gmail.com>
2023-05-14 21:20:32 -0500

    changing JS file

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 $(document).ready(function () {
-    
     $('.saveBtn').on('click', function () {
-    
       var value = $(this).siblings('.description').val();
       var time = $(this).parent().attr('id');
-  
       localStorage.setItem(time, value);
-  
-      $('.notification').addClass('show');
-  
-      setTimeout(function () {
-        $('.notification').removeClass('show');
-      }, 5000);
     });
   
     function hourUpdater() {
-
       var currentHour = dayjs().hour();
   
       $('.time-block').each(function () {
